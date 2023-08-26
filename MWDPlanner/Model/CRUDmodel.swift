6c8469db3a9bb8e468127d299608150cce29b990
suojae <suojae3@gmail.com>
2023-08-26 15:30:10 +0900

    테이블뷰 삭제기능 구현

--- MWDPlanner/Model/CRUDmodel.swift
+++ MWDPlanner/Model/CRUDmodel.swift
@@ -1,53 +1,38 @@
-
-
 import Foundation
 
-// CRUD 함수 제공 클래스
 class TaskService {
 
-    // UserDefaults 저장소내 어디에 저장되었는지 알려주는 위치 key
     private let tasksKey = "tasks"
-    
     static let shared = TaskService()
-    
-    
-    // 파라미터에는 넘겨줄 값을 넣어주기
     var fetchCompletion: (([Task]) -> Void)?
-    
+
     private init() {}
     
-    // MARK: 1
-    //UserDefaults로 부터 Task 객체 배열 가져오기 - 입력
-    func fetchTasks() -> [Task] {
-        //UserDefaults내 주어진 키에 해당하는 데이터 확인하기
-        if let data = UserDefaults.standard.data(forKey: tasksKey),
-           
-            // Task 객체배열로 decode하기
-           let tasks = try? JSONDecoder().decode([Task].self, from: data) {
-            
-            // decode가 완요되면 task를 리턴하기 - 출력
-            return tasks
-        }
-
-        // 만약 가져온 데이터가 비어있거나 디코딩에 실패한다면 빈배열 리턴하기 - 출력
-        return []
-    }
-    
-    // Task 타입의 새로운 데이터를 UserDefaults에 저장하기 - 입력
-    func save(_ task: Task) {
-        // fetchTasks()를 통해 기존 데이터 가져오기
-        var tasks = fetchTasks()
-        
-        // tasks 배열에 새로 받아온 task 첫번째에 insert
-        tasks.insert(task, at: 0)
-        
-        // 이제 다시 encode 해주기
+    private func saveTasksToUserDefaults(_ tasks: [Task]) {
         if let data = try? JSONEncoder().encode(tasks) {
-            // encode가 성공했다면 UserDefaults에 넣어주기
             UserDefaults.standard.set(data, forKey: tasksKey)
-            
-            //MARK: 3
             self.fetchCompletion?(tasks)
         }
     }
+
+    func fetchTasks() -> [Task] {
+        guard let data = UserDefaults.standard.data(forKey: tasksKey),
+              let tasks = try? JSONDecoder().decode([Task].self, from: data)
+        else { return [] }
+        return tasks
+    }
+    
+    func save(_ task: Task) {
+        var tasks = fetchTasks()
+        tasks.insert(task, at: 0)
+        saveTasksToUserDefaults(tasks)
+    }
+    
+    func delete(_ task: Task) {
+        var tasks = fetchTasks()
+        tasks.removeAll { $0 == task }
+        saveTasksToUserDefaults(tasks)
+    }
+
+
 }
--- MWDPlanner/SharedObjects/TaskTableView.swift
+++ MWDPlanner/SharedObjects/TaskTableView.swift
@@ -13,37 +13,57 @@ class TaskTableView: NSObject {
         return tableView
     }()
 
-    
      init(service: TaskService) {
-         self.taskService = service
-       
-
+        self.taskService = service
         super.init()
-         
-         // 클로저를 통해 모델과 연결
-         self.taskService.fetchCompletion = { [weak self] taskArray in
-             self?.tasks = taskArray
-         }
-         tasks = taskService.fetchTasks()
-
         tableView.dataSource = self
         tableView.delegate = self
+         
+        // 클로저를 통해 모델과 연결
+        self.taskService.fetchCompletion = { [weak self] taskArray in
+            self?.tasks = taskArray
+        }
+        tasks = taskService.fetchTasks()
     }
 }
 
 
 extension TaskTableView: UITableViewDataSource, UITableViewDelegate {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let action = UIContextualAction(style: .normal, title: nil) { (action, view, completion) in
+            
+            // userDefaults 데이터 삭제
+            self.taskService.delete(self.tasks[indexPath.row])
+            
+            // table view row 삭제
+            tableView.deleteRows(at: [indexPath], with: .left)
+            completion(true)
+        }
         
+        action.backgroundColor = .black
+        action.image = UIImage(systemName: "trash")
+        
+        let configuration = UISwipeActionsConfiguration(actions: [action])
+        configuration.performsFirstActionWithFullSwipe = true
+        
+        return configuration
+    }
+     
+ 
+
+
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         self.tasks.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "TaskCell", for: indexPath)
-      
         let task = tasks[indexPath.row]
-        print(task)
         cell.textLabel?.text = task.title
         return cell
     }
+    
+   
 }
